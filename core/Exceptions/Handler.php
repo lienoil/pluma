198e9eb50dc92fc428a44a97178778905fd26ca3
John Lioneil Dionisio <john.dionisio1@gmail.com>
2017-08-21 17:45:48 +0800

    added 404 page

--- core/Exceptions/Handler.php
+++ core/Exceptions/Handler.php
@@ -49,13 +49,13 @@ class Handler extends BaseHandler
         }
 
         if ($exception instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
-            // return response()->view("Frontier::errors.404", [
-            //     'error' => [
-            //         'code' => 'NOT_FOUND',
-            //         'message' => $exception->getMessage(),
-            //         'description' => 'The page you requested was not found.',
-            //     ]
-            // ], 404);
+            return response()->view("Frontier::errors.404", [
+                'error' => [
+                    'code' => 'NOT_FOUND',
+                    'message' => $exception->getMessage(),
+                    'description' => 'The page you requested was not found.',
+                ]
+            ], 404);
         }
 
         if (($exception instanceof \ReflectionException) && (auth()->user() && ! auth()->user()->isRoot())) {
@@ -69,13 +69,13 @@ class Handler extends BaseHandler
         }
 
         if ($exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
-            // return response()->view(config('settings.pages.404', 'Frontier::errors.404'), [
-            //     'error' => [
-            //         'code' => 'NOT_FOUND',
-            //         'message' => $exception->getMessage(),
-            //         'description' => config('messages.404'),
-            //     ]
-            // ], 404);
+            return response()->view(config('settings.pages.404', 'Frontier::errors.404'), [
+                'error' => [
+                    'code' => 'NOT_FOUND',
+                    'message' => $exception->getMessage(),
+                    'description' => config('messages.404'),
+                ]
+            ], 404);
         }
 
         if ($exception instanceof \Illuminate\Auth\Access\AuthorizationException) {
--- core/submodules/Frontier/views/errors/404.blade.php
+++ core/submodules/Frontier/views/errors/404.blade.php
@@ -1,9 +1,16 @@
 @extends("Frontier::layouts.public")
 
-@section("head.title", "404 Error")
+@section("head-title", "404 Error")
 
 @section("content")
-    <p>404 {{ $error['code'] }}</p>
-    <p>{{ $error['message'] }}</p>
-    <p>{{ $error['description'] }}</p>
+    @include("Theme::partials.banner")
+
+    <v-layout row wrap>
+        <v-flex xs8 offset-xs2 class="text-xs-center">
+
+            <h1 class="display-6 mt-4 mb-0 accent--text"><strong>{{ __('404 Error') }}</strong></h1>
+            <p class="display-3 mt-0 grey--text">{{ __('Page Not Found') }}</p>
+
+        </v-flex>
+    </v-layout>
 @endsection
