c912de155813d0c68ce467a781a195c7cd86d14a
John Lioneil Dionisio <john.dionisio1@gmail.com>
2018-01-10 14:51:55 +0800

    deleted cache files

--- storage/compiled/views/0f22c849d83b510820ed02c2247a42232c894942.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection("pre-container", ""); ?>
-<?php $__env->startSection("post-container", ""); ?>
-
-<?php $__env->startSection("root"); ?>
-    <?php echo $__env->yieldContent("Template::public.menu"); ?>
-    <?php echo $__env->yieldContent("content"); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make("Theme::layouts.master", array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
