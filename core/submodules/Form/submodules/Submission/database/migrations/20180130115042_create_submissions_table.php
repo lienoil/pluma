649b87ec704af872c8ecf034cf4e099b799fb6ab
Princess Ellen Alto <princessellen0016@yahoo.com>
2018-01-30 20:11:47 +0800

    .

---
+++ core/submodules/Form/submodules/Submission/database/migrations/20180130115042_create_submissions_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Pluma\Support\Migration\Migration;
+use Phinx\Migration\AbstractMigration;
+
+class CreateSubmissionsTable extends Migration
+{
+    /**
+     * The table name.
+     *
+     * @var string
+     */
+    protected $tablename = 'submissions';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if ($this->schema->hasTable($this->tablename)) {
+            return;
+        }
+
+        $this->schema->create($this->tablename, function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type')->nullable();
+            $table->text('results');
+
+            $table->foreign('form_id')->unsigned()->references('id')->on('forms');
+            $table->foreign('user_id')->unsigned()->references('id')->on('users');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->dropIfExists($this->tablename);
+    }
+}
