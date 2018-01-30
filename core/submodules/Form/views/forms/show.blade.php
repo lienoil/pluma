d0f03bbb9906d3931e2513fc75c8dc62f107ddbc
Princess Ellen Alto <princessellen0016@yahoo.com>
2018-01-30 09:02:17 +0800

    .

--- core/submodules/Form/views/forms/show.blade.php
+++ core/submodules/Form/views/forms/show.blade.php
@@ -3,26 +3,48 @@
 @section("content")
     @include("Theme::partials.banner")
 
-    <v-toolbar dark class="elevation-1 grey darken-3">
-        <v-toolbar-title>{{ $resource->name }}</v-toolbar-title>
-    </v-toolbar>
-
     <v-container fluid grid-list-lg>
         <v-layout row wrap>
             <v-flex sm12>
                 <form action="{{$resource->action}}" method="{{ $resource->method }}" {{ $resource->attributes }}>
                     <v-card class="elevation-1">
-                            @foreach ($form->fields as $field)
-                        <v-card-text>
-                            {{ $field->label }}
-                        </v-card-text>
-                        <v-card-text>
-                            <v-text-field label="Type a Message"name="{{ $field->name }}"value="{{ $field->value }}"></v-text-field>
-                        </v-card-text>
-                            @endforeach
+                        <v-toolbar class="elevation-0">
+                            <v-toolbar-title>{{ $resource->name }}</v-toolbar-title>
+                        </v-toolbar>
+
+                        @foreach ($form->fields as $field)
+                            <v-card-text>
+                                {{ $field->label }}
+                            </v-card-text>
+                            <v-card-text>
+                                <v-text-field label="Type a Message" name="{{ $field->name }}" value="{{ $field->value }}"></v-text-field>
+                            </v-card-text>
+
+                            <v-card-text>
+                                <v-radio-group v-model="radio" :mandatory="false">
+                                    <v-radio label="{{ $field->value }}" value="{{ $field->value }}"></v-radio>
+                                </v-radio-group>
+                            </v-card-text>
+                        @endforeach
                     </v-card>
                 </form>
             </v-flex>
         </v-layout>
     </v-container>
 @endsection
+
+
+@push('pre-scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/1.3.4/vue-resource.min.js"></script>
+    <script>
+        Vue.use(VueResource);
+
+        mixins.push({
+            data () {
+                return {
+                    radio: null,
+                };
+            },
+        });
+    </script>
+@endpush
