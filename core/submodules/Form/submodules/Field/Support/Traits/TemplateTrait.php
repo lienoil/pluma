c5b33f3d2c9d2c8a973b276c4e728ae52b7e2885
Princess Ellen Alto <princessellen0016@yahoo.com>
2018-02-02 09:24:10 +0800

    Too lazy to write descriptive message

--- core/submodules/Form/submodules/Field/Support/Traits/TemplateTrait.php
+++ core/submodules/Form/submodules/Field/Support/Traits/TemplateTrait.php
@@ -27,17 +27,17 @@ trait TemplateTrait
      */
     public function template($code = null, $templateFieldName = null)
     {
+        $this->code = $code ?? $this->fieldtype->code;
+
         if (! is_null($templateFieldName)) {
             $this->template = $this->{$templateFieldName};
         } else {
-            $this->template = $this->fieldtype->template;
+            $this->template = $this->fieldtype->where('code', $this->code)->first()->template;
         }
 
         if (is_null($this->template)) {
             $this->template = $this->getDefaultTemplate();
         }
-
-        $this->code = $code;
 
         return $this;
     }
@@ -60,12 +60,8 @@ trait TemplateTrait
     public function render()
     {
         $template = $this->template;
-        $template = preg_replace('/%name%/', $this->name, $template);
-        $template = preg_replace('/%label%/', $this->label, $template);
-        $template = preg_replace('/%tabindex%/', $this->sort, $template);
-        $template = preg_replace('/%type%/', $this->type, $template);
-        $template = preg_replace('/%value%/', $this->value, $template);
-        $template = preg_replace('/%attributes%/', $this->attributed, $template);
+        $template = $this->attachInputTags($template);
+        $template = $this->replaceKeyVariables($template);
         $template = $this->replaceInputElement($template);
 
         return $template;
@@ -96,6 +92,25 @@ trait TemplateTrait
     }
 
     /**
+     * Replaces the Key Variables with actual value.
+     *
+     * @param  string $template
+     * @return string
+     */
+    public function replaceKeyVariables($template)
+    {
+        $template = preg_replace('/%name%/', $this->name, $template);
+        $template = preg_replace('/%label%/', $this->label, $template);
+        $template = preg_replace('/%tabindex%/', $this->sort, $template);
+        $template = preg_replace('/%field_id%/', $this->id, $template);
+        $template = preg_replace('/%type%/', $this->type, $template);
+        $template = preg_replace('/%value%/', $this->value, $template);
+        $template = preg_replace('/%attributes%/', $this->attributed, $template);
+
+        return $template;
+    }
+
+    /**
      * Replaces the input element.
      *
      * @param  string $template
@@ -103,10 +118,13 @@ trait TemplateTrait
      */
     public function replaceInputElement($template)
     {
-        switch ($this->fieldtype->code) {
+        switch ($this->code) {
             case 'vuetify-select':
             case 'vuetify-radio':
             case 'vuetify-checkbox':
+            case 'select':
+            case 'radio':
+            case 'checkbox':
                 $values = explode('|', $this->value);
                 foreach ($values as &$value) {
                     $value = str_replace('*', '', $value);
@@ -119,6 +137,20 @@ trait TemplateTrait
                 return $template;
                 break;
         }
+
+        return $template;
+    }
+
+    /**
+     * Attaches an HTML input tag with the value of the `field_id`
+     * and the field %name%.
+     *
+     * @param  string $template
+     * @return string
+     */
+    public function attachInputTags($template)
+    {
+        $template .= '<input type="hidden" name="%name%[field_id]" value="%field_id%">';
 
         return $template;
     }
---
+++ modules/Yggdrasil/submodules/Course/Support/Traits/CourseResourcePublicTrait.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Course\Support\Traits;
+
+use Illuminate\Http\Request;
+use Menu\Models\Menu;
+use Course\Models\Course;
+
+trait CourseResourcePublicTrait
+{
+    /**
+     * Retrieve list of all resources.
+     *
+     * @param  Illuminate\Http\Request $request
+     * @return Illuminate\Http\Response
+     */
+    public function all(Request $request)
+    {
+        $resources = Course::search($request->all())->paginate();
+
+        return view("Theme::courses.all")->with(compact('resources'));
+    }
+
+    /**
+     * Try to retrieve the resource of the given slug.
+     *
+     * @param  Illuminate\Http\Request $request
+     * @param  string $code
+     * @return Illuminate\Http\Response
+     */
+    public function single(Request $request, $code = null)
+    {
+        $menu = Menu::whereSlug(
+            is_null($code) ? settings('site_home', 'home') : $code
+        );
+
+        if ($menu->exists()) {
+            $menu = $menu->first();
+            $course = Course::codeOrFail($menu->code);
+
+            // Check if template exists.
+            $template = is_null($course->template) ? 'generic' : $course->template;
+            if (view()->exists("Theme::templates.$template")) {
+                return view("Theme::templates.$template")
+                            ->with(compact('course'));
+            }
+
+            // Check if a course exists.
+            if (view()->exists("Theme::courses.{$course->code}")) {
+                return view("Theme::courses.{$course->code}")
+                            ->with(compact('course'));
+            }
+
+            // Default to the index course.
+            return view("Theme::templates.index")->compact('course');
+        }
+
+        // The $code does not exist on the app's menus.
+        // Try if a static file exists for the $code.
+        if (view()->exists("Theme::static.$code")) {
+            return view("Theme::static.$code");
+        }
+
+        // Try the generic Static hintpath
+        if (view()->exists("Static::$code")) {
+            return view("Static::$code");
+        }
+
+        // Finally, give up your dreams.
+        return abort(404);
+    }
+}
