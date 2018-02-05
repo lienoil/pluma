26c90c3d72c9aa25a131996cc70e363f3f635791
Princess Ellen Alto <princessellen0016@yahoo.com>
2018-02-05 10:12:25 +0800

    .

--- modules/Yggdrasil/submodules/Course/views/widgets/enrolled.blade.php
+++ modules/Yggdrasil/submodules/Course/views/widgets/enrolled.blade.php
@@ -1,36 +1,29 @@
 @if (! $resource->enrolled)
     <v-card ref="enroll-card" class="mb-3 elevation-1">
-        <v-toolbar dark dense card class="transparent">
+        <v-toolbar card class="transparent">
             <v-toolbar-title class="subheading page-title">{{ __('Availability') }}</v-toolbar-title>
             <v-spacer></v-spacer>
                 <v-btn dark icon @click="$refs['enroll-card'].remove()"><v-icon>close</v-icon></v-btn>
         </v-toolbar>
+        <v-divider></v-divider>
         {{-- <v-card-media contain height="320px" class="pink lighten-3" src="{{ widgets('enrolled-to-this-course')->backdrop ?? '' }}"></v-card-media> --}}
-        <v-card-text class="subheading">
+        <v-card-text>
             <div class="text-xs-center">
-                <v-icon class="pink--text text--darken-3 display-4">school</v-icon>
+                <v-icon class="grey--text  text--lighten-1 display-4">school</v-icon>
                 <div>
                     @if ($resource->enrolled)
                         {{ __("You are currently enrolled to this course.") }}
                     @else
-                        {{ __("You are not enrolled to this course yet.") }}
+                        <span class="body-1 grey--text text--lighten-1"><em>{{ __("You are not enrolled to this course yet.") }}</em></span>
                     @endif
                 </div>
             </div>
         </v-card-text>
 
-        <v-card-text class="subheading">
-            <div class="text-xs-center">
-                <v-icon>school</v-icon>
-            </div>
-        </v-card-text>
-
         @if (! $resource->enrolled)
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn outline ripple>{{ __("Get Course") }} <sup><v-icon class="caption">add</v-icon></sup></v-btn>
-                <v-spacer></v-spacer>
-            </v-card-actions>
+            <v-card-text class="text-xs-center">
+                <v-btn ripple large primary class="px-4">{{ __("Get Course") }} <sup><v-icon class="caption">add</v-icon></sup></v-btn>
+            </v-card-text>
         @endif
     </v-card>
 @endif
