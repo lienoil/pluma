0093b046d29540fea0a8a0da943991f53d8fb8ff
John Lioneil Dionisio <john.dionisio1@gmail.com>
2018-02-21 18:56:52 +0800

    prep for meging

--- themes/single/views/layouts/admin.blade.php
+++ themes/single/views/layouts/admin.blade.php
@@ -9,6 +9,10 @@
   @include("Theme::partials.breadcrumbs")
 @endpush
 
+@push("after-inner-content")
+  @include("Theme::partials.rightsidebar")
+@endpush
+
 @push("after-content")
   @include("Theme::partials.footer")
 @endpush
