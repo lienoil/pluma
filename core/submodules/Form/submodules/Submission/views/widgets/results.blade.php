d20bd3bba22c700b29f0bb9fdc3cc8a03928685e
NAME <princessellen0016@yahoo.com>
2018-02-27 08:27:09 +0800

    .

--- core/submodules/Form/submodules/Submission/views/widgets/results.blade.php
+++ core/submodules/Form/submodules/Submission/views/widgets/results.blade.php
@@ -130,17 +130,6 @@
                                     fontSize: 18,
                                 }
                             },
-                            tooltips: {
-                              callbacks: {
-                                label: function(tooltipItem, data) {
-                                  let label = data.datasets[tooltipItem.datasetIndex].label;
-                                  let value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
-                                  return label + ': ' + value;
-                                },
-                                footer: function(tooltipItems, data) {
-                                }
-                              }
-                            }
                         }
                     });
                 },
