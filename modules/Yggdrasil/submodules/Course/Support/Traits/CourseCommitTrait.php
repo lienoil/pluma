75ed0c63f42020efe6d32ebfb1ddcbfe9ca3e0f2
Princess Ellen <princessellen0016@yahoo.com>
2018-02-28 08:02:13 +0800

    .

---
+++ modules/Yggdrasil/submodules/Course/Support/Traits/CourseCommitTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Course\Support\Traits;
+
+use Content\Models\Content;
+use Course\Models\User as Student;
+
+trait CourseCommitTrait
+{
+    /**
+     * Commits the changes to `content_user` table.
+     *
+     * @param int $content_id
+     * @return void
+     */
+    public function commit($content_id)
+    {
+        try {
+            $student = Student::find(user()->id);
+            $current = $student->contents()->where('content_id', $content_id)->first();
+            if ($current) {
+                $current->pivot->status = 'done';
+                $current->pivot->save();
+            }
+
+            $next = $student->contents()->where('status', 'pending')->first();
+            if ($next) {
+                $next->pivot->status = 'current';
+                $next->pivot->save();
+            }
+        } catch (\Exception $e) {
+            return dd($e->getMessage());
+        }
+    }
+}
