27b6461068fa23e6b8184561e9d3430df13b3263
Princess Ellen <princessellen0016@yahoo.com>
2018-03-13 12:10:30 +0800

    .

--- modules/Yggdrasil/submodules/Course/views/widgets/getcourse.blade.php
+++ modules/Yggdrasil/submodules/Course/views/widgets/getcourse.blade.php
@@ -14,6 +14,37 @@
                 <v-btn class="primary primary--text px-4" large outline ripple slot="activator">{{ __("Request Course") }}</v-btn>
                 <v-card class="text-xs-center elevation-4">
                     <v-card-text class="pa-5">
+                        <p class="headline ma-2 mb-4"><v-icon round class="warning--text display-4">warning</v-icon></p>
+                        <h2 class="display-1 grey--text text--darken-2 page-title"><strong>{{ __('Are you sure?') }}</strong></h2>
+                        <div class="grey--text text--darken-2">
+                            <span class="mb-3 page-title">{{ __("Are you sure you want to enroll this course?") }} <strong></span>
+                        </div>
+                    </v-card-text>
+                    <v-card-actions class="pa-4">
+                        <v-btn
+                            depressed
+                            large
+                            class="grey--text grey lighten-2 px-4 white--text elevation-0"
+                            @click.native="permission.form.dialog = !permission.form.dialog">
+                            Cancel
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                            depressed
+                            success
+                            large
+                            class="px-4 elevation-0"
+                            @click.stop="permission.form.dialog = false">
+                            Yes
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+
+            <v-dialog v-model="permission.form.dialog" persistent width="500px">
+                {{-- <v-btn class="primary primary--text px-4" large outline ripple slot="activator">{{ __("Request Course") }}</v-btn> --}}
+                <v-card class="text-xs-center elevation-4">
+                    <v-card-text class="pa-5">
                         <p class="headline ma-2 mb-4"><v-icon round class="success--text display-4">check_circle</v-icon></p>
                         <h2 class="display-1 grey--text text--darken-2 page-title"><strong>{{ __('Thank you!') }}</strong></h2>
                         <div class="grey--text text--darken-2">
@@ -22,12 +53,32 @@
                     </v-card-text>
                     <v-card-actions class="py-4">
                         <v-spacer></v-spacer>
-                        <v-btn depressed success large class="px-4 white--text elevation-0" @click.native="enroll.form.dialog = false">Okay</v-btn>
+                        <v-btn
+                            depressed
+                            large
+                            class="px-4 white--text elevation-0"
+                            @click.stop="permission.form.dialog = false">
+                            Okay
+                        </v-btn>
                         <v-spacer></v-spacer>
                     </v-card-actions>
                 </v-card>
             </v-dialog>
             {{-- Dialog pop-up form --}}
+
+            {{-- Dialog for propmt email-template --}}
+            <v-dialog v-model="permission.form.dialog" persistent width="500px">
+                <v-card class="text-xs-center elevation-4">
+                    <v-card-text class="pa-5">
+                        <h2 class="page-title">{{ __('Title') }}</h2>
+                    </v-card-text>
+                    <v-card-actions class="py-4">
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+
+            {{-- Dialog for propmt email-template --}}
 
         </div>
     </v-layout>
@@ -35,9 +86,6 @@
 
 @push('pre-scripts')
     <script src="{{ assets('frontier/vendors/vue/resource/vue-resource.min.js') }}"></script>
-    {{-- <script src="{{ assets('course/scorm/scormdriver.js') }}"></script> --}}
-    {{-- <script src="{{ assets('course/scorm/scorm.6.0.0.js') }}"></script> --}}
-    {{-- <script src="{{ assets('course/scorm/scorm.js') }}"></script> --}}
     <script src="{{ assets('course/scorm/scorm.adapter.api-1.2-2004.js') }}"></script>
     <script>
         Vue.use(VueResource);
@@ -50,6 +98,11 @@
                             dialog: false,
                         }
                     },
+                    permission: {
+                        form: {
+                            dialog: false,
+                        }
+                    }
                 }
             }
         })
