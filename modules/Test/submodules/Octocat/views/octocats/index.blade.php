d948366e9c5d1aabf20177db250af5acf1430926
Princess Ellen <princessellen0016@yahoo.com>
2018-03-17 15:47:18 +0800

    .

--- modules/Test/submodules/Octocat/views/octocats/index.blade.php
+++ modules/Test/submodules/Octocat/views/octocats/index.blade.php
@@ -30,7 +30,7 @@
                         <v-list class="elevation-1">
                             <v-list-tile>
                                 <v-list-tile-content>
-                                    {{ $resource->title }}
+                                    {{ $resource->title }} 09554820744
                                 </v-list-tile-content>
                             </v-list-tile>
                         </v-list>
@@ -83,11 +83,25 @@
                             :label="`Switch 1: ${switch1.toString()}`"
                             v-model="switch1"
                         ></v-switch>
+                        <v-switch
+                            :label="`Switch 1: ${switch1.toString()}`"
+                            v-model="switch1"
+                        ></v-switch>
                     </v-toolbar>
+
+                    <v-toolbar flat class="transparent">
+                        {{ __('Full Courses') }}
+                    </v-toolbar>
+                    <v-spacer></v-spacer>
                     <v-divider></v-divider>
                     <v-card-text>
                         <v-checkbox label="Jane" v-model="selected" value="Jane"></v-checkbox>
                         <v-checkbox label="Joe" v-model="selected" value="Joe"></v-checkbox>
+                    </v-card-text>
+
+                    <v-card-text label="Jane" v-model="selected" value="Jane">
+                        <v-checkbox label="Jane" v-model="selected"></v-checkbox>
+                        <v-checkbox label="Jane" v-model="selected"></v-checkbox>
                     </v-card-text>
 
                     <v-card-texxt>
