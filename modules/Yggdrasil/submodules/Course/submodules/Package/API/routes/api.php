59b22b04fab5a93950c8b530b1d14c16a05b25f3
John Lioneil Dionisio <john.dionisio1@gmail.com>
2018-06-16 22:29:25 +0800

    yggdrasil commit

--- modules/Yggdrasil/submodules/Course/submodules/Package/API/routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::post('packages/upload', '\Package\API\Controllers\PackageController@upload')->name('packages.upload');
-Route::get('packages/paginated', '\Package\API\Controllers\PackageController@paginated')->name('packages.paginated');
-Route::get('packages/search', '\Package\API\Controllers\PackageController@search')->name('packages.search');
