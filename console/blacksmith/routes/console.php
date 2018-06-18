5ae98ba6525726e4d5928ea770dddf63e17b41af
John Lioneil Dionisio <john.dionisio1@gmail.com>
2018-06-18 19:04:55 +0800

    Change default route console command in Blacksmith

--- console/blacksmith/routes/console.php
+++ console/blacksmith/routes/console.php
@@ -6,16 +6,16 @@ use Symfony\Component\Console\Helper\Table;
 use \Symfony\Component\Process\Exception\ProcessFailedException;
 use \Symfony\Component\Process\Process;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+/**
+ *------------------------------------------------------------------------------
+ * Console Routes
+ *------------------------------------------------------------------------------
+ *
+ * This file is where you may define all of your Closure based console
+ * commands. Each Closure is bound to a command instance allowing a
+ * simple approach to interacting with each command's IO methods.
+ *
+ */
 
 Artisan::command('quote:someone', function () {
     $this->comment(Collection::make([
