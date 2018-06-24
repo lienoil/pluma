aafa8542edca078db26222db8496640cd2f8b06e
John Lioneil P. Dionisio <john.dionisio1@gmail.com>
2018-06-24 22:18:33 +0800

    Update blacksmith commands.

--- console/blacksmith/Console/Commands/Furnace/ForgeControllerCommand.php
+++ console/blacksmith/Console/Commands/Furnace/ForgeControllerCommand.php
@@ -6,7 +6,6 @@ use Blacksmith\Support\Console\GeneratorCommand;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Composer;
 use Illuminate\Support\Facades\File;
-use Pluma\Support\Filesystem\Filesystem;
 use Pluma\Support\Modules\Traits\ModulerTrait;
 
 class ForgeControllerCommand extends GeneratorCommand
@@ -20,10 +19,12 @@ class ForgeControllerCommand extends GeneratorCommand
      */
     protected $signature = 'forge:controller
                            {name : The controller to create}
-                           {--m|module=none : Specify the module it belongs to, if applicable}
-                           {--a|admin : Specify the controller extends the Admin Controller}
-                           {--p|public : Specify the controller extends the Public Controller}
-                           {--api : Specify the controller extends the Api Controller}
+                           {--m|module= : Specify the module it belongs to, if applicable.}
+                           {--a|admin : Specify the controller extends the Admin Controller.}
+                           {--p|public : Specify the controller extends the Public Controller.}
+                           {--g|general : Specify the controller extends the General purpose Controller.}
+                           {--api : Specify the controller extends the Api Controller.}
+                           {--model : Generate a resource controller for the given model.}
                            ';
 
     /**
@@ -48,6 +49,13 @@ class ForgeControllerCommand extends GeneratorCommand
     protected $composer;
 
     /**
+     * The module the generated file belongs to.
+     *
+     * @var string
+     */
+    protected $module;
+
+    /**
      * Create a new command instance.
      *
      * @param  \Illuminate\Filesystem\Filesystem  $files
@@ -68,18 +76,31 @@ class ForgeControllerCommand extends GeneratorCommand
      */
     public function handle()
     {
-        if (! $module = $this->input->getOption('module')) {
-            $module = $this->getModulePath(
-                $this->choice("Specify the module the seeder will belong to.", $this->modules());
-            );
-        }
-        dd($module);
-
-        $this->input->setOption('module', $module);
+        $this->qualifyModule();
 
         parent::handle();
 
         $this->composer->dumpAutoloads();
+    }
+
+    /**
+     * Get the module the file belongs to.
+     *
+     * @return string
+     */
+    protected function qualifyModule()
+    {
+        $module = $this->input->getOption('module');
+
+        if (! $module || ! $this->isModule($module)) {
+            $module = $this->choice("Specify the module the seeder will belong to.", $this->modules());
+        }
+
+        $this->module = $this->getModulePath($module);
+
+        $this->input->setOption('module', $this->module);
+
+        return $this->module;
     }
 
     /**
@@ -89,6 +110,120 @@ class ForgeControllerCommand extends GeneratorCommand
      */
     protected function getStub()
     {
-        return blacksmith_path('templates/controllers/Controller.stub');
+        $stub = 'templates/controllers/Controller.stub';
+
+        if ($this->option('admin')) {
+            $stub = 'templates/controllers/AdminController.stub';
+        } elseif ($this->option('public')) {
+            $stub = 'templates/controllers/PublicController.stub';
+        } elseif ($this->option('general')) {
+            $stub = 'templates/controllers/GeneralController.stub';
+        } elseif ($this->option('api')) {
+            $stub = 'templates/controllers/ApiController.stub';
+        }
+
+        return blacksmith_path($stub);
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Controllers';
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * Remove the base controller import if we are already in base namespace.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        $controllerNamespace = $this->getNamespace($name);
+
+        $replace = [];
+
+        if ($this->option('model')) {
+            $replace = $this->buildModelReplacements($replace);
+        }
+
+        $replace["use {$controllerNamespace}\Controller;\n"] = '';
+
+        return str_replace(
+            array_keys($replace), array_values($replace), parent::buildClass($name)
+        );
+    }
+
+    /**
+     * Build the model replacement values.
+     *
+     * @param  array  $replace
+     * @return array
+     */
+    protected function buildModelReplacements(array $replace)
+    {
+        $modelClass = $this->parseModel($this->option('model'));
+
+        if (! class_exists($modelClass)) {
+            if ($this->confirm("A {$modelClass} model does not exist. Do you want to generate it?", true)) {
+                $this->call('forge:model', ['name' => $modelClass]);
+            }
+        }
+
+        return array_merge($replace, [
+            'DummyFullModelClass' => $modelClass,
+            'DummyModelClass' => class_basename($modelClass),
+            'DummyModelVariable' => lcfirst(class_basename($modelClass)),
+        ]);
+    }
+
+    /**
+     * Get the fully-qualified model class name.
+     *
+     * @param  string  $model
+     * @return string
+     */
+    protected function parseModel($model)
+    {
+        if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
+            throw new InvalidArgumentException('Model name contains invalid characters.');
+        }
+
+        $model = trim(str_replace('/', '\\', $model), '\\');
+
+        if (! Str::startsWith($model, $rootNamespace = $this->webApp->getNamespace())) {
+            $model = $rootNamespace.$model;
+        }
+
+        return $model;
+    }
+
+    /**
+     * Get the root namespace for the class.
+     *
+     * @return string
+     */
+    protected function rootNamespace()
+    {
+        return $this->module;
+    }
+
+    /**
+     * Get the destination class path.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getPath($name)
+    {
+        $name = basename($name);
+        return $this->rootNamespace().'/Controllers/'.$name.'.php';
     }
 }
